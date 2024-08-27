d4625e727adde34e9fdfb29f1bdea083af67ad85
Wasim Rashid <wasim.rashid@developers.studio>
2024-08-27 18:54:55 +0500

    changes

---
+++ learn-python/python-lectures/class-15/piaic/my_code/code.py
@@ -0,0 +1,10 @@
+"""
+This module contains the Code class which prints a message when initialized.
+"""
+
+class Code:
+    """A simple class that prints 'my code' upon initialization."""
+
+    def __init__(self) -> None:
+        """Initializes the Code class and prints a message."""
+        print("my code")
