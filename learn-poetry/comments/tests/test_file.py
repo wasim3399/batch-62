ea84998baa8d0389be7a7149ef2a0de6976e67b8
Wasim Rashid <wasim.rashid@developers.studio>
2024-10-07 12:54:21 +0500

    changes

--- learn-poetry/comments/tests/test_file.py
+++
@@ -1,8 +0,0 @@
-from fastapi.testclient import TestClient
-from comments.main import app
-
-def test_file():
-    client = TestClient(app)
-    response = client.get("/test")
-    assert response.status_code == 200
-    assert response.json() == {'message': 'Hello World'}
